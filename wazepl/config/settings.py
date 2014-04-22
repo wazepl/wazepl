acf9305434c56c30232d32e845947a3916af8b26
Patryk Ściborek <patryk@sciborek.com>
2014-04-22 19:08:34 +0200

    Last fixes before release on github

--- wazepl/config/settings.py
+++ wazepl/config/settings.py
@@ -387,14 +387,14 @@ class Production(Common):
 #
     ########## EMAIL
     DEFAULT_FROM_EMAIL = values.Value(
-            'wazepl <noreply@waze.sciborek.com>')
-    EMAIL_HOST = values.Value('smtp.sendgrid.com')
-    EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
-    EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
-    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
-    EMAIL_SUBJECT_PREFIX = values.Value('[wazepl] ', environ_name="EMAIL_SUBJECT_PREFIX")
-    EMAIL_USE_TLS = True
-    SERVER_EMAIL = EMAIL_HOST_USER
+            'wazepl <wazepl@sciborek.com>')
+#    EMAIL_HOST = values.Value('smtp.sendgrid.com')
+#    EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
+#    EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
+#    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
+#    EMAIL_SUBJECT_PREFIX = values.Value('[wazepl] ', environ_name="EMAIL_SUBJECT_PREFIX")
+#    EMAIL_USE_TLS = True
+#    SERVER_EMAIL = EMAIL_HOST_USER
     ########## END EMAIL
 
     ########## TEMPLATE CONFIGURATION
